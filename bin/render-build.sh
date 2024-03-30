8b3b5ddad3e4d50db2f78e3f3690bc12b6e2df0a
Pius <emefiene.pius@yahoo.com>
2024-03-30 06:50:00 -0500

    app

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,8 +8,8 @@ rm -rf public
 npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
-# # builds the back end code
- bundle install
- bundle exec rake db:migrate
- bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
-#
+# # # builds the back end code
+#  bundle install
+#  bundle exec rake db:migrate
+#  bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
+# #
